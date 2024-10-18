21aa363a8caf3d60a3f3dd797c42264a52132f8f
shazzad <hello@shazzad.me>
2024-10-18 21:40:14 +0600

    Added deployer

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function (grunt) {
+  "use strict";
+
+  var pluginVersion = "";
+  var pkgJson = require("./package.json");
+
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+  grunt.getPluginVersion = function () {
+    var p = "shazzad-wp-logs.php";
+    if (pluginVersion == "" && grunt.file.exists(p)) {
+      var source = grunt.file.read(p);
+      var found = source.match(/Version:\s(.*)/);
+      pluginVersion = found[1];
+    }
+    return pluginVersion;
+  };
+
+  grunt.initConfig({
+    pkg: "<json:package.json>",
+    compress: {
+      main: {
+        options: {
+          archive: "build/shazzad-wp-logs.v" + pkgJson.version + ".zip",
+        },
+        files: [
+          { src: "assets/**", dest: "shazzad-wp-logs/" },
+          { src: "includes/**", dest: "shazzad-wp-logs/" },
+          { src: "vendor/**", dest: "shazzad-wp-logs/" },
+          {
+            src: "shazzad-wp-logs.php",
+            dest: "shazzad-wp-logs/",
+          },
+          { src: "index.php", dest: "shazzad-wp-logs/" },
+        ],
+      },
+    },
+    "string-replace": {
+      inline: {
+        files: {
+          "./": ["shazzad-wp-logs.php"],
+        },
+        options: {
+          replacements: [
+            {
+              pattern: "Version: " + grunt.getPluginVersion(),
+              replacement: "Version: " + pkgJson.version,
+            },
+            {
+              pattern: "SWPL_VERSION', '" + grunt.getPluginVersion() + "'",
+              replacement: "SWPL_VERSION', '" + pkgJson.version + "'",
+            },
+          ],
+        },
+      },
+    },
+  });
+
+  grunt.registerTask("version", ["string-replace"]);
+  grunt.registerTask("build", ["version", "compress"]);
+};
